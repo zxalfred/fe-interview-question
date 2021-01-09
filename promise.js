3c4026f76c269bcf5e69340dc0297010fd4ec53b
zxalfred <zxsprout@foxmail.com>
2021-01-09 15:55:29 +0800

    promise

---
+++ promise.js
@@ -0,0 +1,184 @@
+const PENDING = 'PENDING'
+const FULFILLED = 'FULFILLED'
+const REJECTED = 'REJECTED'
+
+function resolvePromise(promise, x, resolve, reject) {
+  if (promise === x) {
+    return reject(new TypeError('Chaining cycle detected for promise'))
+  }
+  let called
+  if ((typeof x === 'object' && x!== null) || typeof x === 'function') {
+    const then = x.then
+    if (typeof then === 'function') {
+      try {
+        then.call(x, y => {
+          if (called) return
+          called = true
+          resolvePromise(promise, y, resolve, reject)
+        }, r => {
+          if (called) return
+          called = true
+          reject(r)
+        })
+      } catch (e) {
+        if (called) return
+        called = true
+        reject(e)
+      }
+    } else {
+      resolve(x)
+    }
+  } else {
+    resolve(x)
+  }
+}
+
+class MyPromise {
+  constructor(executor) {
+    this.status = PENDING
+    this.value = undefined
+    this.reason = undefined
+    this.onResolvedCallbacks = []
+    this.onRejectedCallbacks = []
+
+    const resolve = (value) => {
+      if (value instanceof MyPromise) {
+        return value.then(resolve, reject)
+      }
+      if (this.status === PENDING) {
+        this.status = FULFILLED
+        this.value = value
+        this.onResolvedCallbacks.forEach(fn => fn(value))
+      }
+    }
+    const reject = (value) => {
+      if (this.status === PENDING) {
+        this.status = REJECTED
+        this.reason = value
+        this.onRejectedCallbacks.forEach(fn => fn(value))
+      }
+    }
+    try {
+      executor(resolve, reject)
+    } catch (e) {
+      reject(e)
+    }
+  }
+
+  then(onFulfilled, onRejected) {
+    onFulfilled = typeof onFulfilled === 'function' ? onFulfilled : v => v
+    onRejected = typeof onRejected === 'function' ? onRejected : e => { throw e }
+
+    const promise2 = new MyPromise((resolve, reject) => {
+      if (this.status === FULFILLED) {
+        setTimeout(() => {
+          try {
+            const x = onFulfilled(this.value)
+            resolvePromise(promise2, x, resolve, reject)
+          } catch (e) {
+            reject(e)
+          }
+        }, 0)
+      }
+      if (this.status === REJECTED) {
+        setTimeout(() => {
+          try {
+            const x = onRejected(this.reason)
+            resolvePromise(promise2, x, resolve, reject)
+          } catch (e) {
+            reject(e)
+          }
+        }, 0);
+      }
+      if (this.status === PENDING) {
+        this.onResolvedCallbacks.push(() => {
+          setTimeout(() => {
+            try {
+              const x = onFulfilled(this.value)
+              resolvePromise(promise2, x, resolve, reject)
+            } catch (e) {
+              reject(e)
+            }
+          }, 0);
+        })
+        this.onResolvedCallbacks.push(() => {
+          setTimeout(() => {
+            try {
+              const x = onRejected(this.reason)
+              resolvePromise(promise2, x, resolve, reject)
+            } catch (e) {
+              reject(e)
+            }
+          }, 0);
+        })
+      }
+    })
+
+    return promise2
+  }
+
+  catch(onRejected) {
+    this.then(null, onRejected)
+  }
+
+  finally(callback) {
+    return this.then(data => {
+      return MyPromise.resolve(callback()).then(() => data)
+    }, reason => {
+      return MyPromise.resolve(callback()).then(() => { throw reason })
+    })
+  }
+
+  all(values) {
+    if (!Array.isArray(values)) {
+      const type = typeof values
+      return new TypeError(`TypeError: ${type} ${values} is not iterable`)
+    }
+    return new Promise((resolve, reject) => {
+      const resultArr = []
+      let orderIndex = 0
+      const processResultByKey = (value, index) => {
+        resultArr[index] = value
+        if (++orderIndex === values.length) {
+          resolve(resultArr)
+        }
+      }
+      for (let i = 0; i < values.length; i++) {
+        const value = values[i]
+        if (value && typeof value.then === 'function') {
+          value.then(res => {
+            processResultByKey(value, i)
+          }, reject)
+        } else {
+          processResultByKey(value, i)
+        }
+      }
+    })
+  }
+
+  race(promises) {
+    return new MyPromise((resolve, reject) => {
+      for (let i = 0; i < promises.length; i++) {
+        const val = promises[i]
+        if (val && typeof val.then === 'function') {
+          val.then(resolve, reject)
+        } else {
+          resolve(val)
+        }
+      }
+    })
+  }
+
+  static resolve(data) {
+    return new MyPromise(resolve => {
+      resolve(data)
+    })
+  }
+
+  static reject(reason) {
+
+    return new MyPromise((resolve, reject) => {
+      reject(reason)
+    })
+  }
+}
