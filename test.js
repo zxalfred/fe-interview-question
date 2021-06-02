e448d287867af5763523b3da1cba16aae1835f64
zxalfred <zxsprout@foxmail.com>
2021-06-02 19:27:15 +0800

    update

---
+++ test.js
@@ -0,0 +1,11 @@
+const PENDING = 'PENDING'
+const FULFILLED = 'FULFILLED'
+const REJECTED = 'REJECTED'
+
+class MyPromise {
+  constructor(executor) {
+    this.status = PENDING
+    this.value = undefined
+    this.reason = undefined
+  }
+}
